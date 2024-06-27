d34e76d637a32b6bddcd9124bafc500eb24589fa
coco-netizen <bluejack911@hotmail.com>
2024-06-27 22:34:34 +0800

    Add files via upload

---
+++ diy-part2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argonne/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+sed -i 's/OpenWrt/Coco-Router/g' package/base-files/files/bin/config_generate
+
+#修改默认密码
+sed -i 's/root::0:0:99999:7:::/root:$1$3ZL2YxPA$NmwUNvXNWmpdhlQV8xQcp1:19872:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
